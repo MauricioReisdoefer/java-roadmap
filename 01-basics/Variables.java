7ac5b57f7bc25cef4e94656a1da92856308bec50
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-05 17:13:26 -0300

    LESSON: Creating Variables.java

---
+++ 01-basics/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int idade = 25;           // inteiro
+        double salario = 3500.50; // ponto flutuante
+        boolean ativo = true;     // verdadeiro/falso
+        char sexo = 'M';          // caractere
+        String nome = "Maurício"; // texto
+       
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Ativo: " + ativo);
+        System.out.println("Sexo: " + sexo);
+    }
+}
