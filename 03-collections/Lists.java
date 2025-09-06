33b405d67d54b3c0ce3e654604fc600182b79481
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-06 15:59:15 -0300

    LESSON: ArrayLists in java

---
+++ 03-collections/Lists.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Lists {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Ana");
+        nomes.add("Bruno");
+        nomes.add("Ana");
+
+        System.out.println(nomes);
+    }
+}
