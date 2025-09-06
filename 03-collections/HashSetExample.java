a5f948a96ea55dfc9abd3087555acd2413c5abbe
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-06 16:02:56 -0300

    LESSON: HashSets in java

---
+++ 03-collections/HashSetExample.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> frutas = new HashSet<>();
+        frutas.add("Maçã");
+        frutas.add("Banana");
+        frutas.add("Maçã"); // será ignorado
+
+        System.out.println(frutas);
+    }
+}
